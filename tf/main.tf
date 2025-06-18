9287139acc090678ec81c0861831ce0767d7d044
attilafekete73 <125585722+attilafekete73@users.noreply.github.com>
2025-06-18 08:17:21 +0000

    things with workflows

--- tf/main.tf
+++ tf/main.tf
@@ -61,12 +61,7 @@ resource "azurerm_linux_web_app" "app" {
   # For GitHub deployment, use GitHub Actions or other deployment methods.
 }
 
-# Add GitHub deployment for the webapp
-resource "azurerm_app_service_source_control" "app_source" {
-  app_id             = azurerm_linux_web_app.app.id
-  branch             = "main"
-  repo_url           = "https://github.com/attilafekete73/cphtmlhelloworld"
-}
+
 
 #############################
 # Autoscale Settings
