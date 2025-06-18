997c1f6b86da48f8413726329144ebfb62518e5f
attilafekete73 <feketeattila2008@gmail.com>
2025-06-18 16:39:15 +0200

    subscription_ID fix

--- tf/main.tf
+++ tf/main.tf
@@ -1,4 +1,20 @@
+variable "subscription_id" {
+  description = "get subscription ID"
+  type        = string
+}
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
   backend "azurerm" {
     resource_group_name  = "tfstate"
     storage_account_name = "tfstate28091" # replace with actual name
