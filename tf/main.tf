59ce9a6014fa09d2888dbfce0f03bc7ba3af08a1
attilafekete73 <feketeattila2008@gmail.com>
2025-07-02 18:17:37 +0200

    fix

--- tf/main.tf
+++ tf/main.tf
@@ -36,7 +36,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "tfstate"
-    storage_account_name = "tfstate28091"
+    storage_account_name = "tfstate3628800"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
--- tf/us.tf
+++ tf/us.tf
@@ -53,7 +53,7 @@ resource "azurerm_app_service_source_control" "app_source-us" {
   repo_url = "https://github.com/attilafekete73/Cloud-Programming-with-Azure"
   depends_on = [
     azurerm_app_service_source_control_token.github,
-    azurerm_azurerm_linux_web_app.app-asia
+    azurerm_azurerm_linux_web_app.app-us
   ]
 
 }
